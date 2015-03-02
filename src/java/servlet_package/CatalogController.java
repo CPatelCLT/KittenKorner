d67412cf38e8e4a486704ed997b360edf201b31d
cpatel36 <cpatel36@uncc.edu>
2015-03-02 11:13:56 -0500

    Added some stuff to the catalog controller

--- src/java/servlet_package/CatalogController.java
+++ src/java/servlet_package/CatalogController.java
@@ -6,8 +6,11 @@
 package servlet_package;
 
 import java.io.*;
+import java.util.ArrayList;
+import java_files.Product;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import temp_db.ProductDB;
 /*import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;*/
@@ -26,9 +29,15 @@ public class CatalogController extends HttpServlet {
         
     }
     
+    @Override
     protected void doGet(HttpServletRequest request, 
                           HttpServletResponse response) 
                           throws ServletException, IOException {
         doPost(request, response);
+        ProductDB pdb = new ProductDB();
+        ArrayList<Product> prodList = pdb.getProdList();
+        request.setAttribute("products", pdb);
+        RequestDispatcher rd = request.getRequestDispatcher("/catalog.jsp");
+        rd.forward(request, response);
     }  
 }
