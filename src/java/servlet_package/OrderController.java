1d0568bab1c32f605e5f4b37f2dd8546b64b9d58
Eric <xenomisis@gmail.com>
2015-04-16 15:58:10 -0400

    Fixed the my orders link issue when not signed in

--- src/java/servlet_package/OrderController.java
+++ src/java/servlet_package/OrderController.java
@@ -159,8 +159,9 @@ public class OrderController extends HttpServlet {
 //            String usr = request.getUserPrincipal().getName();
 //            User u = udb.getUserByEmail(usr);
             if (u==null){
-                RequestDispatcher dispatch = request.getRequestDispatcher("/secure/signedin.jsp");
-                dispatch.forward(request, response);
+                response.sendRedirect("secure/signedin.jsp");
+//                RequestDispatcher dispatch = request.getRequestDispatcher("/secure/signedin.jsp");
+//                dispatch.forward(request, response);
             }
             else{
 //                User u = udb.getUserByEmail(usr);
