e08809ea1d72a09fe1f485ff2637c2241fd95f33
Chirag <Chirag@192.168.1.101>
2015-04-17 04:34:21 -0400

    Modified the mail stuff a little

--- src/java/java_beans/OrderConfirmation.java
+++ src/java/java_beans/OrderConfirmation.java
@@ -16,14 +16,11 @@ import javax.mail.internet.*;
  * @author chirag
  */
 public class OrderConfirmation {
-    public void sendConfirmation(int ordId) {
-        OrderDB odb = new OrderDB();
+    public void sendConfirmation(Order ord) {
         UserDB udb = new UserDB();
-        // TODO Add getOrder(int ordID) to OrderDB
-        Order currOrder = new Order(); //odb.getOrder(ordId);
+        Order currOrder = ord;
         User currUser = udb.getUser(currOrder.getUser().getUserID());
         String to = currUser.getEmailAddress();
-        // TODO Change to actual email information
         String from = "kittenkorner@measurementcontrols.com";
         String host = "mail.measurementcontrols.com";
         Properties properties = System.getProperties();
